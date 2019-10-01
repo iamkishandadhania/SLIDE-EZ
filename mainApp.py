0425c1a58c3da053f1d808b55b66a1014acf3909
Kishan Dadhania <43320721+iamkishandadhania@users.noreply.github.com>
2019-10-01 19:46:07 +0530

    Add files via upload
    Final Touches

---
+++ mainApp.py
@@ -0,0 +1,30 @@
+import eel
+import test3
+import appUtils as au
+# expose python functions to javascript
+# these functions can be called from JS in index.html
+@eel.expose
+def initApp():
+    pass
+
+@eel.expose
+def closeApp():
+    print("close")
+    import newdata
+    newdata.get_result()
+    import sys
+    sys.exit()
+    
+@eel.expose
+def addPoint():
+    uResponse = au.sysListen()
+    # print(uResponse)
+    eel.opEnd(uResponse)
+    test3.fn(uResponse)
+
+try:
+    eel.init('web', allowed_extensions=['.js','.html', ".jpg", ".py"])
+    eel.start('index.html', size=(1024,720))
+except (SystemExit, MemoryError, KeyboardInterrupt):
+    pass
+print("Application Terminated")
